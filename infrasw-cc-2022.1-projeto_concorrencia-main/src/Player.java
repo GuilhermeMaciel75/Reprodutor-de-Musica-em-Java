6616bb74f9a09e3688d70cee10a8dc5de8cf99de
GuilhermeMaciel75 <guilhermemacielmelo75@hotmail.com>
2022-09-01 13:55:01 -0300

    Musica adicionada e reproduzindo

--- infrasw-cc-2022.1-projeto_concorrencia-main/src/Player.java
+++ infrasw-cc-2022.1-projeto_concorrencia-main/src/Player.java
@@ -10,6 +10,10 @@ import javax.swing.event.MouseInputAdapter;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Player {
 
@@ -28,13 +32,80 @@ public class Player {
 
     private PlayerWindow window;
 
-    private String [][] musics;
+    private final ReentrantLock lock = new ReentrantLock();
+    private final Condition lockCondition = lock.newCondition();
+
+    private String[][] musics = new String[1][0];
+    private Song[] lista_songs = new Song[1];
+    int stopPlayNow = 0;
 
     private int currentFrame = 0;
 
-    private final ActionListener buttonListenerPlayNow = e -> {};
+    private final ActionListener buttonListenerPlayNow = e -> {
+        currentFrame = 0;
+        this.window.setPlayingSongInfo(lista_songs[0].getTitle(), lista_songs[0].getAlbum(), lista_songs[0].getArtist());
+
+        if(this.bitstream != null){
+            try {
+                bitstream.close();
+            } catch (BitstreamException ex) {
+                throw new RuntimeException(ex);
+            }
+
+            device.close();
+        }
+
+        try {
+            this.device = FactoryRegistry.systemRegistry().createAudioDevice();
+        } catch (JavaLayerException ex) {
+            throw new RuntimeException(ex);
+        }
+        try {
+            this.device.open(this.decoder = new Decoder());
+        } catch (JavaLayerException ex) {
+            throw new RuntimeException(ex);
+        }
+        try {
+            this.bitstream = new Bitstream(lista_songs[0].getBufferedInputStream());
+        } catch (FileNotFoundException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        while(true){
+            //this.window.setTime(, lista_songs[0].getMsLength());
+            try {
+                playNextFrame();
+            } catch (JavaLayerException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+    };
     private final ActionListener buttonListenerRemove = e -> {};
-    private final ActionListener buttonListenerAddSong = e -> {};
+    private final ActionListener buttonListenerAddSong = e -> {
+
+        Song music;
+
+        try {
+            music = this.window.openFileChooser();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        } catch (BitstreamException ex) {
+            throw new RuntimeException(ex);
+        } catch (UnsupportedTagException ex) {
+            throw new RuntimeException(ex);
+        } catch (InvalidDataException ex) {
+            throw new RuntimeException(ex);
+        }
+
+        musics[0] = music.getDisplayInfo();
+
+        this.window.setQueueList(musics);
+
+        lista_songs[0] = music;
+
+
+
+    };
     private final ActionListener buttonListenerPlayPause = e -> {};
     private final ActionListener buttonListenerStop = e -> {};
     private final ActionListener buttonListenerNext = e -> {};
